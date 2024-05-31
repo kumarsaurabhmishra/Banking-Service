f4ae2e73ba6a1272e8f7891369c53d40d04534cb
Kumar Saurabh Mishra <abcsaurabhxyz3@gmail.com>
2024-06-01 00:00:09 +0530

    Initial commit

---
+++ banking-api-gateway/src/test/java/com/saurabh/finance/BankingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ banking-core-service-registry/src/test/java/com/saurabh/finance/BankingCoreServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingCoreServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
